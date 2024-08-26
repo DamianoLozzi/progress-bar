f808b09f6a306a38882b8b7ba05731d630796ecb
Damiano Lozzi <damianolozzi1989@gmail.com>
2024-08-26 16:59:41 +0200

    chore: update package name to progress_bar

--- progress_bar/__init__.py
+++ progress_bar/__init__.py
@@ -1,4 +1,4 @@
-from .custom_progress import get_default_format, get_progress_bar, get_async_progress_bar
+from .progress_bar import get_default_format, get_progress_bar, get_async_progress_bar
 
 __all__ = ['get_default_format', 'get_progress_bar', 'get_async_progress_bar']
 
